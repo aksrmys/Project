ea368bf62095487f6482a250825fd57eef70f561
Rümeysa Akış <80596533+aksrmys@users.noreply.github.com>
2023-06-09 02:22:43 +0300

    Update index.js

--- Project3/src/js/index.js
+++ Project3/src/js/index.js
@@ -1,73 +1,25 @@
-// Function to clear the main element
-function clearMain() {
-    var main = document.querySelector('main');
-    main.innerHTML = '';
-}
-
-// Function to create HTML elements for the speaker data
-function createSpeakerElements(data) {
-    var main = document.querySelector('main');
-
-    // Create container div
-    var container = document.createElement('div');
-    container.classList.add('container');
-    main.appendChild(container);
-
-    // Create content div
-    var content = document.createElement('div');
-    content.classList.add('content');
-    container.appendChild(content);
-
-    // Create title
-    var title = document.createElement('h1');
-    title.textContent = data.title;
-    content.appendChild(title);
-
-    // Create image
-    var image = document.createElement('img');
-    image.src = data.image;
-    content.appendChild(image);
-
-    // Create name
-    var name = document.createElement('h2');
-    name.textContent = data.name;
-    content.appendChild(name);
-
-    // Create text
-    var text = document.createElement('p');
-    text.textContent = data.text;
-    content.appendChild(text);
-}
-
-// Click event handler for sidebar links
-function linkClickHandler(event) {
-    event.preventDefault();
-
-    var link = event.target;
-    var fileName = link.getAttribute('href');
-
-    // Remove the '#' character from the filename
-    fileName = fileName.slice(1) + '.json';
-
-    // Retrieve the data from the JSON file
-    fetch(fileName)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            // Clear the main element
-            clearMain();
-
-            // Create HTML elements for the speaker data
-            createSpeakerElements(data.options[0]);
-        })
-        .catch(function(error) {
-            console.log('Error:', error);
-        });
-}
-
-// Add click event listeners to sidebar links
-var links = document.querySelectorAll('.left-area a');
-links.forEach(function(link) {
-    link.addEventListener('click', linkClickHandler);
-});
+$(document).ready(function() {
+    $("#boxes a").click(function(event) {
+      event.preventDefault(); 
+  
+      var speakerTitle = $(this).attr("title");
+      
+      // Build the name of the JSON file based on the speaker's title
+      var jsonFileName = "src/json_files/" + speakerTitle + ".json";
+      
+      // Clear the elements 
+      $("#suggest").empty();
+  
+      // Retrieve the JSON data for the speaker
+      $.getJSON(jsonFileName, function(data) {
+        var speaker = data.speakers[0]; 
+        var speakerHtml = '<h1>' + speaker.title + '</h1>';
+        speakerHtml += '<img src="' + speaker.image + '">';
+        speakerHtml += '<h2>' + speaker.month + '<br>' + speaker.speaker + '</h2>';
+        speakerHtml += '<p>' + speaker.text + '</p>';
+  
+        // Append the speaker HTML elements 
+        $("suggest").append(speakerHtml);
+      });
+    });
+  });
